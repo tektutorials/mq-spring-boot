43f4cd09acc4324b9781dfa83dab075dcc2b434c
Jaikrat Singh <tektutorialfeedback@gmail.com>
2021-08-05 13:40:05 +0530

    Initial commit

---
+++ src/test/java/com/mq/spring/boot/MqSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mq.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
